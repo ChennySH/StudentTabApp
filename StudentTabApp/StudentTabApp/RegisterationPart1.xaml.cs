04491007967eb20aeb04a1902827091da0d305f8
User <User@LAB3-17>
2021-03-10 13:31:55 +0200

    Add project files.

---
+++ StudentTabApp/StudentTabApp/RegisterationPart1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentTabApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterationPart1 : ContentPage
+    {
+        public RegisterationPart1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ StudentTabApp/StudentTabApp/RegisterationPart3.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace StudentTabApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterationPart3 : ContentPage
+    {
+        public RegisterationPart3()
+        {
+            InitializeComponent();
+        }
+    }
+}
